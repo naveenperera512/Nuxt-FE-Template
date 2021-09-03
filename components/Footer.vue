b00bae40b2d6ef8284047e7fe6a6d3902734682f
naveen10qbit <naveen@10qbit.com>
2021-09-03 18:30:07 +0530

    build

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<script>
+/**
+ * Footer Component
+ */
+export default {}
+</script>
+
+<template>
+<!-- Footer Start -->
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                {{ new Date().getFullYear()}} &copy; Chairsyde theme by <a href="">Chairsyde</a>
+            </div>
+ 
+        </div>
+    </div>
+</footer>
+<!-- end Footer -->
+</template>
---
+++ pages/education_content/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>education content</div>
+</template>
+
+
+
